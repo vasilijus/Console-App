991403a443bf9e324a9d617da9aa0c365894d853
vasilijus <vasiliokas@gmail.com>
2022-12-29 22:59:34 +0000

    t

--- Console-APPS1/Program.cs
+++ Console-APPS1/Program.cs
@@ -33,9 +33,6 @@ namespace MyNamespace
             {
                 ConsoleApp1 app = new ConsoleApp1();
             }
-
-            IsAllDigitsP dig = new IsAllDigitsP();
-
             
         }
 
@@ -143,33 +140,6 @@ namespace MyNamespace
         public String Name;
         public string greeting() {
             return "Hello ,"+this.Name.ToUpper()+"!!!";
-        }
-    }
-
-    class IsAllDigitsP
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Enter a integer");
-            string s = Console.ReadLine();
-
-            if (!IsAllDigits(s))
-            {
-                Console.WriteLine("This is not a number");
-            }
-            else
-            {
-                int n = Int32.Parse(s);
-                Console.WriteLine("2 * {0} = {1}", n, 2*n);
-            }
-            Console.WriteLine("Press [Enter] for " +
-                "finishing the app...");
-            Console.Read();
-        }
-
-        public static bool IsAllDigits(string raw)
-        {
-            
         }
     }
 
--- README.md
+++
@@ -1,4 +0,0 @@
-# Console App
-
-❯ dotnet add reference ../PrimeService/PrimeService.csproj
-❯ dotnet sln add PrimeService/PrimeService.csproj
